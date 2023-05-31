a6724093bf0bb6184274d5cba689c281e4e85664
Florian Marx <83638230+florian-mx@users.noreply.github.com>
2023-05-31 23:18:55 +0200

    Update prepare-tests.sh

--- prepare-tests.sh
+++ prepare-tests.sh
@@ -1,3 +1,3 @@
 cd /tmp/willigis-info-lk-tests/src/main/java
-mv $(find $GITHUB_WORKSPACE -name "src")/* .
-cp /tmp/willigis-info-lk-tests/* $GITHUB_WORKSPACE/
+mv -r $(find $GITHUB_WORKSPACE -name "src")/* .
+cp -r /tmp/willigis-info-lk-tests/* $GITHUB_WORKSPACE/
